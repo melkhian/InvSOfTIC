cf2b059b54653a646199ffa4b20f9abc4d10a946
melkhian <40581362+melkhian@users.noreply.github.com>
2019-02-20 09:20:36 -0500

    form Aplicaciones
    Se da formato tipo tabla a XV: Documentación

--- backend/views/aplicaciones/Tabs/documentacion.php
+++ backend/views/aplicaciones/Tabs/documentacion.php
@@ -13,85 +13,112 @@ use backend\models\Tipos;
 <br>
 <h1 align="center">XV. DOCUMENTACIÓN</h1>
 <br><br>
-    <?= $form->field($model, 'TiposId_fk29')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-    <?= $form->field($model, 'TiposId_fk30')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
 
-    <?= $form->field($model, 'TiposId_fk31')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk32')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk33')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk34')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk35')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk36')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk37')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk38')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk39')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk40')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk41')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk42')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk43')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk44')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk45')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk46')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk47')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk48')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk49')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk50')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk51')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk52')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk53')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'TiposId_fk54')
-    ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 50')->all(),'TiposValo','TiposDesc'))?>
-
-    <?= $form->field($model, 'AppUbic')->textInput(['maxlength' => true]) ?>
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+    <table>
+      <tr>
+        <th>Nombre del documento asociado</th>
+        <th>¿Se entregó?</th>
+        <th>¿Se aprobó?</th>
+        <th>Medio</th>
+      </tr>
+      <tr>
+        <td>Plan de proyecto</td>
+        <td>  <?= $form->field($model, 'TiposId_fk29')
+          ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk30')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk31')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Definición y Alcance</td>
+        <td>  <?= $form->field($model, 'TiposId_fk32')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk33')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk34')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Documento de requerimientos</td>
+        <td>  <?= $form->field($model, 'TiposId_fk35')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk36')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td>  <?= $form->field($model, 'TiposId_fk37')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Documento de diseño</td>
+        <td> <?= $form->field($model, 'TiposId_fk38')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk39')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk40')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Documentos de Pruebas</td>
+        <td> <?= $form->field($model, 'TiposId_fk41')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk42')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk43')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Manual Técnico y de instalación</td>
+        <td> <?= $form->field($model, 'TiposId_fk44')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk45')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk46')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Manual de Administración</td>
+        <td> <?= $form->field($model, 'TiposId_fk47')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk48')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk49')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>Manual de Usuario</td>
+        <td> <?= $form->field($model, 'TiposId_fk50')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk51')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk52')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 48')->all(),'TiposValo','TiposDesc'))?></td>
+      </tr>
+      <tr>
+        <td>¿Se entregó Medio digital con la información de la aplicación?</td>
+        <td> <?= $form->field($model, 'TiposId_fk53')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'TiposId_fk54')
+            ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 46')->all(),'TiposValo','TiposDesc'))?></td>
+        <td> <?= $form->field($model, 'AppUbic')->textInput(['maxlength' => true]) ?></td>
+      </tr>
+    </table>
+    <br>
 
     <?= $form->field($model, 'TiposId_fk55')
     ->radioList(ArrayHelper::map(Tipos::find()->where('tipoid_fk = 49')->all(),'TiposValo','TiposDesc'))?>
